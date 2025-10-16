6d36ee13f04379c43ee077b51c0debbd08ea9811
Hamza GBADAMASSI <hamzath.gbadamassi@epitech.eu>
2025-10-16 17:58:40 +0100

    [feat] Ajout des nécessaires pour le mot de passe

--- coding-api/database/migrations/0001_01_01_000000_create_users_table.php
+++ coding-api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('required|name');
+            $table->string('required|email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('required|password|min:8');
             $table->rememberToken();
             $table->timestamps();
         });
